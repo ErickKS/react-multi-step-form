ccc492c8e3b25717eee202a15c249cc829bd273c
Erick <erick.kuwahara79@gmail.com>
2022-10-03 12:44:14 -0300

    Fix base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://erickks.github.io/multi-step-form-react",
+  base: "https://erickks.github.io/multi-step-form-react/",
   build: {
     outDir: 'build'
   },
